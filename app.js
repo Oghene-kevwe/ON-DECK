c08b45febd87eb3e0658276448945ba8a33dbb46
Idise praise <Idisepraise@gmail.com>
2022-06-11 17:16:24 +0100

    2

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-let toggleBtn = document.querySelector(".menu-btn")
-let closeBtn = document.querySelector(".close-btn")
-let navLinks = document.querySelector(".nav-links")
+let toggleBtn = document.querySelector(".menu-btn");
+let closeBtn = document.querySelector(".close-btn");
+let navLinks = document.querySelector(".nav-links");
 
-toggleBtn.addEventListener("click",function(){
-    navLinks.classList.toggle("show-links")
-})
+toggleBtn.addEventListener("click", function () {
+  navLinks.classList.toggle("show-links");
+});
 
-closeBtn.addEventListener("click",function(){
-    navLinks.classList.remove("show-links")
-})
+closeBtn.addEventListener("click", function () {
+  navLinks.classList.remove("show-links");
+});
 
+const product = document.querySelectorAll(".product-link");
+const navItem = document.querySelector(".nav-links-items");
 
-
-
-
+product.forEach(function () {
+  const productItem = document.createElement("span");
+  const items = navItem.outerHTML;
+  productItem.innerHTML = items;
+  productItem.classList.add("product-item");
+  product[0].appendChild(productItem);
+});
