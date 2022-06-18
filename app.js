e4d59bc0ec3a851e2d4c38c258eb80fa35862a34
Idise praise <Idisepraise@gmail.com>
2022-06-18 23:27:17 +0100

    modification

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+//  Nav-Links
+
 let toggleBtn = document.querySelector(".menu-btn");
 let closeBtn = document.querySelector(".close-btn");
 let navLinks = document.querySelector(".nav-links");
+const linksContainer = document.querySelector(".links-container");
 
 toggleBtn.addEventListener("click", function () {
-  navLinks.classList.toggle("show-links");
+  const navLinksHeight = navLinks.getBoundingClientRect().height;
+  const LinksHeight = linksContainer.getBoundingClientRect().height;
+
+  if (navLinksHeight === 0) {
+    navLinks.style.height = `${LinksHeight}px`;
+  } else {
+    navLinks.style.height = 0;
+  }
 });
 
 closeBtn.addEventListener("click", function () {
-  navLinks.classList.remove("show-links");
+  navLinks.style.height = 0;
 });
+
+// Desktop Links
 
 const product = document.querySelectorAll(".product-link");
 const navItem = document.querySelector(".nav-links-items");
